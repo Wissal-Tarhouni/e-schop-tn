6b8eaea9683757cd8fba68e6c7ce2f4bf7aea11e
Walid ZEDDINI <walid.zeddini@gmail.com>
2021-01-10 21:11:05 +0100

    Update EshopApplication.java

--- src/main/java/tn/rnu/isi/eschop/EshopApplication.java
+++ src/main/java/tn/rnu/isi/eschop/EshopApplication.java
@@ -1,7 +1,6 @@
 package tn.rnu.isi.eschop;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -11,10 +10,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class EshopApplication extends SpringBootServletInitializer {
 
-	private static final Logger log = LoggerFactory.getLogger(EshopApplication.class);
-
-	
-	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(EshopApplication.class, args);
 	}
