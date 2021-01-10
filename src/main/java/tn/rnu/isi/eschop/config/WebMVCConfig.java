c77b37ab91a30830fb824c2b25c4f9f7dfee5a97
Walid ZEDDINI <walid.zeddini@gmail.com>
2021-01-10 20:52:08 +0100

    Update WebMVCConfig.java

--- src/main/java/tn/rnu/isi/eschop/config/WebMVCConfig.java
+++ src/main/java/tn/rnu/isi/eschop/config/WebMVCConfig.java
@@ -2,25 +2,18 @@ package tn.rnu.isi.eschop.config;
 
 import java.util.Locale;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Description;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
-import org.thymeleaf.spring5.ISpringTemplateEngine;
-import org.thymeleaf.spring5.SpringTemplateEngine;
-import org.thymeleaf.templateresolver.ITemplateResolver;
+
 
 
 
@@ -34,11 +27,9 @@ import org.thymeleaf.templateresolver.ITemplateResolver;
  * Java configuration file that is used for Spring MVC and Thymeleaf
  * configurations
  */
-public class WebMVCConfig implements WebMvcConfigurer,  ApplicationContextAware {
+public class WebMVCConfig implements WebMvcConfigurer {
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) {
-    }
+ 
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -50,18 +41,7 @@ public class WebMVCConfig implements WebMvcConfigurer,  ApplicationContextAware 
 
     }
     
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-    }
-    
-    @Bean
-    @Description("Spring Message Resolver")
-    public ResourceBundleMessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("messages");
-        return messageSource;
-    }
+   
 
     @Bean
     public LocaleResolver localeResolver() {
