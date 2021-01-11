feba00a25b5b0dd748c7e1c5d0e363fb7e9b0779
Walid ZEDDINI <walid.zeddini@gmail.com>
2021-01-11 22:27:25 +0100

    juste test pour SSII

--- src/main/java/tn/rnu/isi/eschop/config/WebMVCConfig.java
+++ src/main/java/tn/rnu/isi/eschop/config/WebMVCConfig.java
@@ -38,6 +38,8 @@ public class WebMVCConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
         registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
         registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/static/fonts/");
+//        registry.addResourceHandler("resources/**").addResourceLocations("/resources/");
+
 
     }
     
